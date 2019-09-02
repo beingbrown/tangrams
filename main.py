5385cc711d87da1c5e468609054b3e5fda977edc
randy brown <randy@beingbrown.net>
2019-09-01 21:49:58 -0500

    read in the file in three forms
    in order to properly parse the image, three different versions of
    it will be used.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import cv2
+
+def get_color_images(input_image):
+    high_poly = cv2.imread(input_image)
+    # Use Otsu's method here ?
+    gray_image = cv2.cvtColor(high_poly, cv2.COLOR_BGR2GRAY)
+    ycbcr_image = cv2.cvtColor(high_poly, cv2.COLOR_RGB2YCrCb)
+    return high_poly, gray_image, ycbcr_image
 
 def main(*args, **kwargs):
     input_image = kwargs['input_image']
