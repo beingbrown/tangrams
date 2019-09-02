b82dc6a1e582e7f5ada67dcf6ae992c76f49861b
randy brown <randy@beingbrown.net>
2019-09-01 21:40:33 -0500

    remove the boilerplate code
    instead of approaching this from detection, a better MVP is a lowpoly
    representation of an image.

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from imageai.Detection import ObjectDetection
-import os
-
-execution_path = os.getcwd()
-
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
-detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
-
-for eachObject in detections:
-    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
 
 def main(*args, **kwargs):
     input_image = kwargs['input_image']
