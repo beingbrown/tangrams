d2943921213b8df2fe9ea880b9c0eee82f0b3296
randy brown <randy@beingbrown.net>
2019-09-03 22:38:28 -0500

    provide a cli invocation
    in order to test the tangrams lib, a direct invocation is defined. It
    expects both the file to be processed and the target to save the result
    to. This does not actually conform with the output method, so it will
    not work.

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ def main(*args, **kwargs):
     input_image = kwargs['input_image']
     output_image = kwargs['output_image']
     # handle errors here
+
+# invoked with tangrams input_image output_image
+if __name__ == '__main__':
+    create_output_image(*sys.argv[1:])
